03d3e87ab8aabbf9864f172c98a3fc9a98cbd53f
Stepan Protsak <stephan.protsack@gmail.com>
2025-07-12 17:22:29 +0200

    Remove version module

--- sklmatcher/__init__.py
+++ sklmatcher/__init__.py
@@ -2,11 +2,9 @@
 # License: BSD 3 clause
 
 from ._template import TemplateClassifier, TemplateEstimator, TemplateTransformer
-from ._version import __version__
 
 __all__ = [
     "TemplateEstimator",
     "TemplateClassifier",
     "TemplateTransformer",
-    "__version__",
 ]
